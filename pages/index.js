4d7f2a0a66de9c79f7a8205156cb4b7d4da63312
Triptig22 <traptitrgour02@gmail.com>
2023-05-26 13:59:27 +0530

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,78 @@
+/* eslint-disable react/jsx-key */
+import { A11y, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  EventsBanner,
+  FeaturesBanner,
+  HeroBanner,
+  Newsletter,
+  Product,
+} from "../components";
+import { client } from "../lib/client";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+const Home = ({ products }) => {
+  return (
+    <>
+      <HeroBanner />
+      <EventsBanner />
+
+      <div className="products-outer-container">
+        <div className="subtitle">
+          <span>PRODUCTS</span>
+          <h2>Check What We Have</h2>
+        </div>
+        <Swiper
+          breakpoints={{
+            // width >= 300
+            300: {
+              slidesPerView: 1,
+              spaceBetween: 100,
+            },
+            // width >= 1000
+            1000: {
+              slidesPerView: 2,
+              spaceBetween: 0,
+            },
+            // width >= 1260
+            1260: {
+              slidesPerView: 3,
+              spaceBetween: 0,
+            },
+          }}
+          modules={[Navigation, A11y]}
+          spaceBetween={0}
+          slidesPerView={3}
+          navigation
+        >
+          <div className="products-container">
+            {products?.map((product) => (
+              <SwiperSlide>
+                <Product key={product._id} product={product} />
+              </SwiperSlide>
+            ))}
+          </div>
+        </Swiper>
+      </div>
+
+      <FeaturesBanner />
+      <Newsletter />
+    </>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  // const bannerQuery = '*[_type == "banner"]';
+  // const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products },
+  };
+};
+
+export default Home;
